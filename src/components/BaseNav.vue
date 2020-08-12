9068c1fbaa44d1d7e2e33132e48ddc43a0d22ca4
Jeric <ijrodrin13@gmail.com>
2020-08-12 13:47:35 +0800

    navbar

--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -6,36 +6,42 @@
         <div class="collapse navbar-collapse ml-3 " id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home </a>
+                    <a class="nav-link text-white text-uppercase" href="#" style="font-size: medium"><b> Kimchi </b> </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link text-white text-uppercase" href="#" style="font-size: medium"><b> Empanda </b></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link text-white text-uppercase" href="#" style="font-size: medium"><b> Kimbap </b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white text-uppercase" href="#" style="font-size: medium"><b> Chicken Wings </b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white text-uppercase disabled" href="#" style="font-size: medium"><b> Samgyupsal </b></a>
                 </li>
             </ul>
         </div>
-        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-            <div class="form-group mb-0">
-                <base-input placeholder="Search Menu"
-                            class="input-group-alternative"
-                            alternative=""
-                            addon-right-icon="fas fa-search">
-                </base-input>
-            </div>
-        </form>
+<!--        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">-->
+<!--            <div class="form-group mb-0">-->
+<!--                <base-input placeholder="Search"-->
+<!--                            class="input-group-alternative"-->
+<!--                            alternative=""-->
+<!--                            addon-right-icon="fas ">-->
+<!--                </base-input>-->
+<!--            </div>-->
+<!--        </form>-->
     </nav>
 </template>
 <script>
-    import BaseInput from "./BaseInput";
+    // import BaseInput from "./BaseInput";
     export default {
         name: "base-nav",
-        components: {BaseInput}
+        // components: {BaseInput}
     }
 </script>
 <style>
-    nav{
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    .navbar-link:hover {
+        background: rgba(79, 31, 3, 0.8);
     }
 </style>
--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,27 +1,26 @@
 <template>
     <div>
-        <base-sidebar id="mySidebar" @click="sidebar.sidebarOpenClose(sidebar.sidebarStatus)"/>
-        <div id="content" class="main">
-            <base-nav class="navbar"></base-nav>
+<!--        <base-sidebar id="mySidebar" @click="sidebar.sidebarOpenClose(sidebar.sidebarStatus)"/>-->
+        <div id="content" class="main bg-warning">
+            <base-nav class="navbar-top navbar-horizontal"></base-nav>
         </div>
     </div>
 </template>
 <script>
     import BaseNav from "../components/BaseNav";
-    import BaseSidebar from "../components/BaseSizeBar";
     export default {
-        components: {BaseSidebar, BaseNav},
+        components: {BaseNav},
         data() {
             return{
                 sidebar: {
                     sidebarStatus: true,
                     openNav() {
                         document.getElementById("mySidebar").style.width = "250px";
-                        //document.getElementById("content").style.marginLeft = "250px";
+                        document.getElementById("content").style.marginLeft = "250px";
                     },
                     closeNav() {
                         document.getElementById("mySidebar").style.width = "10px";
-                        //document.getElementById("content").style.marginLeft= "0px";
+                        document.getElementById("content").style.marginLeft= "0px";
                     },
                     sidebarOpenClose(status) {
                         console.log(status)
@@ -40,7 +39,6 @@
 </script>
 <style>
     .main {
-        background: #cb9207;
         transition: margin-left .5s;
         padding: 16px;
         color: black;
